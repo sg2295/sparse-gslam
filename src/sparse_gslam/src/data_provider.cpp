d804d546daa42cc272ec84eea7c477ba45d4636c
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-04-18 13:40:57 +0100

    Test: no filter last

--- src/sparse_gslam/src/data_provider.cpp
+++ src/sparse_gslam/src/data_provider.cpp
@@ -276,9 +276,9 @@ class EV3DataProvider : public DataProvider {
                 iss >> bearing.at(j);
             // ! Choose what filtering method we will use !
             // 1) N-th value filter (get 1st entry, susceptible to noise)
-            full_range.at(i) = nth_filter<0>(bearing) / scaling;
+            // full_range.at(i) = nth_filter<0>(bearing) / scaling;
             // 2) N-th value filter (get 25th entry, susceptible to noise)
-            // full_range.at(i) = nth_filter<24>(bearing) / scaling;
+            full_range.at(i) = nth_filter<24>(bearing) / scaling;
             // 3) Average filter (incorporates outliers and skews results)
             // full_range.at(i) = average_filter(bearing) / scaling;
             // 4) Median filter (effectively removes outliers & impulse noise)
